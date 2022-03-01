e63b8ddd4aee3c15593f4c813ed5d7f77380e9d2
AnitaBhagashetti <anitabhagashetti7377@gmail.com>
2022-03-01 20:27:59 +0530

    favourite_movie assignment submission

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,13 +2,13 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
-    movies=[]
-    if(operation==="add")
+    
+    if(operation=="add")
     {
         movies.push(movie)
 
     }
-    else if(operation==="remove")
+    else if(operation=="remove")
     {
         movies.pop()
     }
